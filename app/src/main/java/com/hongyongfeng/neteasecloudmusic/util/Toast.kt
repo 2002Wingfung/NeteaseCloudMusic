c61ac4600d27311462437d5c4ec190888065e3c7
Wingfung Hung <1039377283@qq.com>
2023-07-13 14:45:28 +0800

    feat(NavigationView):新建了侧滑菜单

---
+++ app/src/main/java/com/hongyongfeng/neteasecloudmusic/util/Toast.kt
@@ -0,0 +1,11 @@
+package com.hongyongfeng.neteasecloudmusic.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun String.showToast(context:Context,duration: Int=Toast.LENGTH_SHORT){
+    Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
+}
+fun Int.showToast(context:Context,duration: Int=Toast.LENGTH_SHORT){
+    Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
+}

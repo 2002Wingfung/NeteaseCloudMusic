f241e9d3b1295e4ac84772dc713a631e08d9623e
Wingfung Hung <1039377283@qq.com>
2023-07-19 12:30:45 +0800

    feat(player):完成了唱片cd的显示

---
+++ Player/src/main/java/com/hongyongfeng/player/utli/Player.kt
@@ -0,0 +1,10 @@
+package com.hongyongfeng.player.utli
+
+class Player {
+    private fun initMediaPlayer(){
+//        val assetManager=assets
+//        val fd =assetManager.openFd("music.mp3")
+//        mediaPlayer.setDataSource(fd.fileDescriptor,fd.startOffset,fd.length)
+//        mediaPlayer.prepare()
+    }
+}
--- app/src/main/java/com/hongyongfeng/neteasecloudmusic/ui/app/PlayerActivity.kt
+++ app/src/main/java/com/hongyongfeng/neteasecloudmusic/ui/app/PlayerActivity.kt
@@ -1,7 +1,12 @@
 package com.hongyongfeng.neteasecloudmusic.ui.app
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
+import android.view.animation.LinearInterpolator
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -10,10 +15,10 @@ import androidx.lifecycle.ViewModelProvider
 import com.hongyongfeng.neteasecloudmusic.base.BaseActivity
 import com.hongyongfeng.neteasecloudmusic.databinding.ActivityPlayerBinding
 import com.hongyongfeng.neteasecloudmusic.network.APIResponse
+import com.hongyongfeng.neteasecloudmusic.network.api.PlayerInterface
 import com.hongyongfeng.neteasecloudmusic.util.StatusBarUtils
 import com.hongyongfeng.neteasecloudmusic.util.showToast
 import com.hongyongfeng.neteasecloudmusic.viewmodel.PublicViewModel
-import com.hongyongfeng.neteasecloudmusic.network.api.PlayerInterface
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -26,7 +31,21 @@ public class PlayerActivity :BaseActivity<ActivityPlayerBinding,ViewModel>(
         ViewModelProvider(this)[PublicViewModel::class.java]
     }
     private var albumId: Int=0
+    private lateinit var mAnimator: ObjectAnimator
 
+//    private var handler=Handler(Looper.getMainLooper()){
+//
+//    }
+    private var handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            if (msg.what==0){
+                mAnimator.pause()
+            }else{
+                mAnimator.resume()
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bundle = intent.extras;
@@ -48,6 +67,29 @@ public class PlayerActivity :BaseActivity<ActivityPlayerBinding,ViewModel>(
         transparentNavBar(this)
         initView(binding, StatusBarUtils.getStatusBarHeight(this as AppCompatActivity)+5)
         initListener()
+
+    // Object target:目标对象，
+    // String propertyName:指定要改变对象的什么属性，这个属性名要求在对应对象中必须有对应的public的PsetPropertyName的方法。如上面的rotation就要求ImageView中必须有setRotation方法才行。
+    // float... values:一系列这个属性将会到达的值
+        mAnimator= ObjectAnimator.ofFloat(binding.imgAlbum, "rotation", 0f, 720f)
+    // Object target:目标对象，
+    // String propertyName:指定要改变对象的什么属性，这个属性名要求在对应对象中必须有对应的public的PsetPropertyName的方法。如上面的rotation就要求ImageView中必须有setRotation方法才行。
+    // float... values:一系列这个属性将会到达的值
+    // 设置一次动画的时间
+    // 设置一次动画的时间
+        mAnimator.duration = 19000
+    // 设置插值器，用来控制变化率
+    // 设置插值器，用来控制变化率
+        mAnimator.interpolator = LinearInterpolator()
+    // 设置重复的次数，无限
+    // 设置重复的次数，无限
+        mAnimator.repeatCount = ObjectAnimator.INFINITE
+        mAnimator.start()
+        mAnimator.setupStartValues()
+        //当音频文件加载好之后就start
+
+    // 在合适的位置调用 mAnimator.pause()方法进行暂停操作
+
     }
     private fun initView(binding: ActivityPlayerBinding, dp:Int){
         val lp = binding.layoutActionBar.layoutParams as ConstraintLayout.LayoutParams
@@ -108,11 +150,24 @@ public class PlayerActivity :BaseActivity<ActivityPlayerBinding,ViewModel>(
         }
     }
     private fun initListener() {
+        var count=0
         binding.btnBack.setOnClickListener {
             finish()
         }
         binding.icPlay.setOnClickListener {
-            "play".showToast(this)
+            if (count%2==0){
+
+                handler.sendEmptyMessageDelayed(0, 500)
+
+                "暂停".showToast(this)
+
+            }else{
+                handler.sendEmptyMessageDelayed(1, 500)
+
+                "播放".showToast(this)
+            }
+            count++
+
         }
         binding.icNext.setOnClickListener {
             "next".showToast(this)
